ffd08d5ddc1f99b36697a5669034c4075686233c
Archange16 <asathoud16@gmail.com>
2025-05-29 08:27:26 +0100

    pied de page

--- components/layout/footers/footer-three.jsx
+++ components/layout/footers/footer-three.jsx
@@ -54,79 +54,39 @@ const FooterThree = () => {
         <>
         <div className="footer__three">
             <div className="container pt-80 pb-80">
-                <div className="footer__three-top">
-                    <h3>Rejoignez notre communauté sociale !</h3>
-                    <div className="footer__three-top-social">
-                        <Social />
-                    </div>
-                </div>
-                <div className="row gy-4 justify-content-between">
-                    <div className="col-xl-4 col-md-6 col-sm-7 xl-mb-30">
-                        <div className="footer__three-widget">
-                            <div className="footer__three-widget-about">
-                                <Link href="/"><img src={logo.src} alt="image" /></Link>
-                                <p>Lancez votre business ou projet pro avec style 🚀 ! Coaching, formations et conseils pour aligner vos ambitions, vos talents et vos valeurs. <br/> Prêt à transformer vos idées en succès ?
-                                </p>
-                                <h6>Inscrivez-vous à notre Newsletter</h6>
-                                <form onSubmit={handleSubmit}>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        placeholder="Votre e-mail"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                    />
-                                    <button type="submit" disabled={loading}>
-                                        {loading ? 'Envoi...' : <i className="fas fa-paper-plane"></i>}
-                                    </button>
-                                </form>
-                                {message && (
-                                    <div style={{ color: message.includes('succès') ? 'green' : 'red' }}>
-                                        {message}
-                                    </div>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-md-6 col-sm-5 sm-mb-30">
+               {/*  <div className="row gy-4 justify-content-between"> */}
+                
+                    <div className="col-xl-12 col-md-6 col-sm-5 sm-mb-30">
                         <div className="footer__three-widget border-one">
-                            <h4 className="ml-60">Nos Formations</h4>
                             <div className="footer__three-widget-solution">
                                 <ul>
-									<li><Link href={`/business`}><i className="far fa-chevron-double-right"></i>Création d’Entreprise</Link></li>
-									<li><Link href={`/services`} ><i className="far fa-chevron-double-right"></i>Voir le catalogue complet</Link></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-md-6 col-sm-5 sm-mb-30">
-                        <div className="footer__three-widget border-one">
-                            <h4 className="ml-60">Ressources​</h4>
-                            <div className="footer__three-widget-solution">
-                                <ul>
-									<li><Link href="/services"><i className="far fa-chevron-double-right"></i>Formations</Link></li>
-									<li><Link href="/blog"><i className="far fa-chevron-double-right"></i>Blog</Link></li>
+									<li><Link href="https://formations-dentaire.fr">Accueil</Link></li>
+									<li><Link href="https://formations-dentaire.fr/#Solutions">Solutions</Link></li>
+									<li><Link href="https://formations-dentaire.fr/#formation">Formation</Link></li>
+									<li><Link href="/categories">Categories</Link></li>
+									<li><Link href="https://formations-dentaire.fr/#Temoignage">Témoignage</Link></li>
 									<li><Link href="testimonial"></Link></li>
-									<li><Link href="about"><i className="far fa-chevron-double-right"></i>A propos</Link></li>
+									<li><Link href="https://formations-dentaire.fr/#Contact">Contact</Link></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     
-                </div>
+                {/* </div> */}
             </div>
             <div className="copyright__one two">
                 <div className="container">
                     <div className="row justify-content-between copyright__one-container-area">
                         <div className="col-xl-5 col-lg-6"> 
                             <div className="copyright__one-left">
-                                <p>© Kevin Attallah 2025</p>
+                                <p>Copyright © Kevin 
+                                    <Link href="https://formations-dentaire.fr" target="_blank">Kelaj Formation Dentaire.</Link>All Rights Reserved</p>
                             </div>
                         </div>
                         <div className="col-xl-5 col-lg-6">
                             <div className="copyright__one-right">
-                                <Link href="https://meet.brevo.com/kevin-a2ta2l/presentation" target="_blank">Prendre RDV</Link>
+                                <Link href="https://meet.brevo.com/kevin-a2ta2l/presentation" target="_blank">POLITIQUE DE COOKIES</Link> |
+                                 <Link href="https://meet.brevo.com/kevin-a2ta2l/presentation" target="_blank"> Conditions générales de vente</Link>
                             </div>
                         </div>
                     </div>
